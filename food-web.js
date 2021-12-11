dc72f4551764ebd76965dba8a18adbeb54dfcb5a
Anil Kumar Soni <93093591+anil9818@users.noreply.github.com>
2021-12-11 17:49:13 +0530

    Add files via upload

---
+++ food-web.js
@@ -0,0 +1,137 @@
+function home(){
+    document.getElementById('home').style.backgroundColor= 'greenyellow';
+    document.getElementById('home').style.borderRadius= '15px';
+    document.getElementById('home').style.color= 'white';
+    document.getElementById('home').style.fontWeight= 'bold';
+    document.getElementById('about').style.backgroundColor= 'transparent';
+    document.getElementById('about').style.color= 'black';
+    document.getElementById('about').style.fontWeight= 'normal';
+    document.getElementById('dishes').style.backgroundColor= 'transparent';
+    document.getElementById('dishes').style.color= 'black';
+    document.getElementById('dishes').style.fontWeight= 'normal';
+    document.getElementById('menu').style.backgroundColor= 'transparent';
+    document.getElementById('menu').style.color= 'black';
+    document.getElementById('menu').style.fontWeight= 'normal';
+    document.getElementById('review').style.backgroundColor= 'transparent';
+    document.getElementById('review').style.color= 'black';
+    document.getElementById('review').style.fontWeight= 'normal';
+    document.getElementById('order').style.backgroundColor= 'transparent';
+    document.getElementById('order').style.color= 'black';
+    document.getElementById('order').style.fontWeight= 'normal';
+}
+
+function about(){
+    document.getElementById('about').style.backgroundColor= 'greenyellow';
+    document.getElementById('about').style.borderRadius= '15px';
+    document.getElementById('about').style.color= 'white';
+    document.getElementById('about').style.fontWeight= 'bold';
+    document.getElementById('home').style.backgroundColor= 'transparent';
+    document.getElementById('home').style.color= 'black';
+    document.getElementById('home').style.fontWeight= 'normal';
+    document.getElementById('dishes').style.backgroundColor= 'transparent';
+    document.getElementById('dishes').style.color= 'black';
+    document.getElementById('dishes').style.fontWeight= 'normal';
+    document.getElementById('menu').style.backgroundColor= 'transparent';
+    document.getElementById('menu').style.color= 'black';
+    document.getElementById('menu').style.fontWeight= 'normal';
+    document.getElementById('review').style.backgroundColor= 'transparent';
+    document.getElementById('review').style.color= 'black';
+    document.getElementById('review').style.fontWeight= 'normal';
+    document.getElementById('order').style.backgroundColor= 'transparent';
+    document.getElementById('order').style.color= 'black';
+    document.getElementById('order').style.fontWeight= 'normal';
+}
+
+function dishes(){
+    document.getElementById('dishes').style.backgroundColor= 'greenyellow';
+    document.getElementById('dishes').style.borderRadius= '15px';
+    document.getElementById('dishes').style.color= 'white';
+    document.getElementById('dishes').style.fontWeight= 'bold';
+    document.getElementById('about').style.backgroundColor= 'transparent';
+    document.getElementById('about').style.color= 'black';
+    document.getElementById('about').style.fontWeight= 'normal';
+    document.getElementById('home').style.backgroundColor= 'transparent';
+    document.getElementById('home').style.color= 'black';
+    document.getElementById('home').style.fontWeight= 'normal';
+    document.getElementById('menu').style.backgroundColor= 'transparent';
+    document.getElementById('menu').style.color= 'black';
+    document.getElementById('menu').style.fontWeight= 'normal';
+    document.getElementById('review').style.backgroundColor= 'transparent';
+    document.getElementById('review').style.color= 'black';
+    document.getElementById('review').style.fontWeight= 'normal';
+    document.getElementById('order').style.backgroundColor= 'transparent';
+    document.getElementById('order').style.color= 'black';
+    document.getElementById('order').style.fontWeight= 'normal';
+}
+
+function menu(){
+    document.getElementById('menu').style.backgroundColor= 'greenyellow';
+    document.getElementById('menu').style.borderRadius= '15px';
+    document.getElementById('menu').style.color= 'white';
+    document.getElementById('menu').style.fontWeight= 'bold';
+    document.getElementById('about').style.backgroundColor= 'transparent';
+    document.getElementById('about').style.color= 'black';
+    document.getElementById('about').style.fontWeight= 'normal';
+    document.getElementById('dishes').style.backgroundColor= 'transparent';
+    document.getElementById('dishes').style.color= 'black';
+    document.getElementById('dishes').style.fontWeight= 'normal';
+    document.getElementById('home').style.backgroundColor= 'transparent';
+    document.getElementById('home').style.color= 'black';
+    document.getElementById('home').style.fontWeight= 'normal';
+    document.getElementById('review').style.backgroundColor= 'transparent';
+    document.getElementById('review').style.color= 'black';
+    document.getElementById('review').style.fontWeight= 'normal';
+    document.getElementById('order').style.backgroundColor= 'transparent';
+    document.getElementById('order').style.color= 'black';
+    document.getElementById('order').style.fontWeight= 'normal';
+}
+
+function review(){
+    document.getElementById('review').style.backgroundColor= 'greenyellow';
+    document.getElementById('review').style.borderRadius= '15px';
+    document.getElementById('review').style.color= 'white';
+    document.getElementById('review').style.fontWeight= 'bold';
+    document.getElementById('about').style.backgroundColor= 'transparent';
+    document.getElementById('about').style.color= 'black';
+    document.getElementById('about').style.fontWeight= 'normal';
+    document.getElementById('dishes').style.backgroundColor= 'transparent';
+    document.getElementById('dishes').style.color= 'black';
+    document.getElementById('dishes').style.fontWeight= 'normal';
+    document.getElementById('menu').style.backgroundColor= 'transparent';
+    document.getElementById('menu').style.color= 'black';
+    document.getElementById('menu').style.fontWeight= 'normal';
+    document.getElementById('home').style.backgroundColor= 'transparent';
+    document.getElementById('home').style.color= 'black';
+    document.getElementById('home').style.fontWeight= 'normal';
+    document.getElementById('order').style.backgroundColor= 'transparent';
+    document.getElementById('order').style.color= 'black';
+    document.getElementById('order').style.fontWeight= 'normal';
+}
+
+function order(){
+    document.getElementById('order').style.backgroundColor= 'greenyellow';
+    document.getElementById('order').style.borderRadius= '15px';
+    document.getElementById('order').style.color= 'white';
+    document.getElementById('order').style.fontWeight= 'bold';
+    document.getElementById('about').style.backgroundColor= 'transparent';
+    document.getElementById('about').style.color= 'black';
+    document.getElementById('about').style.fontWeight= 'normal';
+    document.getElementById('dishes').style.backgroundColor= 'transparent';
+    document.getElementById('dishes').style.color= 'black';
+    document.getElementById('dishes').style.fontWeight= 'normal';
+    document.getElementById('menu').style.backgroundColor= 'transparent';
+    document.getElementById('menu').style.color= 'black';
+    document.getElementById('menu').style.fontWeight= 'normal';
+    document.getElementById('review').style.backgroundColor= 'transparent';
+    document.getElementById('review').style.color= 'black';
+    document.getElementById('review').style.fontWeight= 'normal';
+    document.getElementById('home').style.backgroundColor= 'transparent';
+    document.getElementById('home').style.color= 'black';
+    document.getElementById('home').style.fontWeight= 'normal';
+}
+
+// $(document).ready(function(){
+//     $("#dishes").click(function(){
+//         $("body").html("<a href='menu'></a>")
+//     });
+// });
